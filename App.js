725f1ceaa8f5f07bedc4673725e791d3e50a4d8b
Weskimo <samuel.hughes4@btinternet.com>
2022-03-17 15:55:02 +0000

    firebase added

--- App.js
+++ App.js
@@ -1,5 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import * as firebase from 'firebase';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkRKmNY7if3q5z2_pMC7kEn50CMy1McAE",
+  authDomain: "esportsteammanagement.firebaseapp.com",
+  projectId: "esportsteammanagement",
+  storageBucket: "esportsteammanagement.appspot.com",
+  messagingSenderId: "64960786238",
+  appId: "1:64960786238:web:952f7550816303a488066f",
+  measurementId: "G-MR2S1WWCTJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   return (
