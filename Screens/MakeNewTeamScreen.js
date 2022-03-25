709c516887c5c4701e8f6c8b66188b49dcde4f0d
Weskimo <samuel.hughes4@btinternet.com>
2022-03-25 01:01:27 +0000

    added func button to the make new team screen so that it makes not only a new team but also a new sub collection with documents that have their unique keys as the user id. I can use this later to get the infomation and the correct privilalgies.

--- Screens/MakeNewTeamScreen.js
+++ Screens/MakeNewTeamScreen.js
@@ -9,40 +9,96 @@ import { authent } from '../db/firestore.js';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getAuth } from "firebase/auth";
-import { getDoc } from 'firebase/firestore';
+import { getDoc , setDoc} from 'firebase/firestore';
 import { StyleSheet } from 'react-native';
 
 
 const MakeNewTeamScreen = () => {
 
+    const navigation = useNavigation();
+
     const [userId, setUserId] = useState('')
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [bio, setBio] = useState('')
+    const [teamId, setTeamId] = useState('')
 
 
     useEffect( async () => {
-        
-    })
+        const id = await AsyncStorage.getItem('@UserId')
+        setUserId(id)
+    }) 
+
+    useEffect (() => {
+        setTeamId(userId + name)
+    },[name])
 
     const createTeam = async () => {
-        const myDoc = doc(db, "Teams");
+        
+        const myDoc = doc(db, "Teams", teamId);
         const docData = {
-            "teamOwnerId": userId,
-            "firstName": firstName,
-            "lastName": lastName,
-            "email": email
+            "name": name,
+            "email": email,
+            "bio": bio
         }
         setDoc(myDoc,docData)
         .then(()=>{
+            
             alert("Team created");
+            // navigation.navigate("MyTeams")
+            
        })
         .catch((error)=>{
             alert(error.message);
+            
       })
     }
+
+    const createTeamUsers = async () => {
+        const myDoc = doc(db, 'Teams', teamId, "Users", userId );
+        const docData = {
+            "teamOwnerId": userId,
+            "admin": true
+            
+        }
+        setDoc(myDoc,docData)
+        .then(()=>{
+            alert("Users created");
+           // navigation.navigate("MyTeams")
+            
+       })
+        .catch((error)=>{
+            alert(error.message);
+            
+      })
+    }
+
+
+    // const messageRef = doc(db, "rooms", "roomA", "messages", "message1");
 
     return (
         <SafeAreaView>
         <Text>MakeNewTeamScreen </Text>
-      
+        <TextInput
+        placeholder='Team Email here...'
+        value={email}
+        onChangeText={text => setEmail(text)}
+        />
+        <TextInput
+        placeholder='Team Name here...'
+        value={name}
+        onChangeText={text => setName(text)}
+        />
+        <TextInput
+        placeholder='Team Bio (50 words max)...'
+        value={bio}
+        onChangeText={text => setBio(text)}
+        />
+        <Button title="Make team" onPress={createTeam}/>
+        <Button title="Make Users" onPress={createTeamUsers}/>
+
+       
         
 
         
