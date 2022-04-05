f6bbb3ced63c2a83c09026a866b346aff7533dbe
Weskimo <samuel.hughes4@btinternet.com>
2022-04-05 20:43:35 +0100

    now have it so image can be uploaded and then also redownlaoded and displayed to the screen

--- Screens/ImagepickerComp.js
+++ Screens/ImagepickerComp.js
@@ -2,13 +2,24 @@ import React, {Component, useState , useEffect} from 'react';
 import {View, Text, TextInput, Button, SafeAreaView, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useNavigation } from '@react-navigation/native';
+import { db, storage } from '../db/firestore';
+import { getStorage, ref, uploadBytes, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+
 
 export default function ImagePickerComp() {
 
   const navigation = useNavigation();
 
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedUploadImage, setSelectedUploadImage] = useState(null);
 
+  const [selectedProfileImage, setSelectedProfileImage] = useState('');
+
+  const storageRef = ref(storage, "Images/image.jpg")
+
+  const down = ref(storage,"gs://esportsteammanagement.appspot.com/Images/image.jpg")
+
+  // Create a reference from a Google Cloud Storage URI
+  const gsReference = ref(storage, 'gs://esportsteammanagement.appspot.com/Images');
 
   let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -23,43 +34,93 @@ export default function ImagePickerComp() {
     if (pickerResult.cancelled === true) {
         return;
       }
-  
-      setSelectedImage({ localUri: pickerResult.uri });
+      const img = await fetch(pickerResult.uri)
+      const bytes = await img.blob();
+
+      await uploadBytes(storageRef, bytes)
+      setSelectedUploadImage({ localUri: pickerResult.uri });
     }
+
+   
+
+  const getDownloadImage = async () => {  // Get the download URL
+    getDownloadURL(down)
+    .then((url) => {
+      
+      setSelectedProfileImage(url);
+      console.log(url)
+      console.log(selectedProfileImage)
+    })
+    .catch((error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/object-not-found':
+          // File doesn't exist
+          break;
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+
+        // ...
+
+        case 'storage/unknown':
+          // Unknown error occurred, inspect the server response
+          break;
+      }
+    });
+  } 
+  
     
-    if (selectedImage !== null) {
-  return (
-    <View>
-      
-      <Image
-      source={{ uri: selectedImage.localUri }}
-      style={styles.thumbnail}
-    />
-      <Text>
-        To share a photo from your phone with a friend, just press the button below!
-      </Text>
+    if (selectedUploadImage !== null) {
+      return (
+        <View>
+          
+          <Image
+          source={{ uri: selectedUploadImage.localUri }}
+          style={styles.thumbnail}
+          />
 
-      <TouchableOpacity onPress={openImagePickerAsync}>
-        <Text>Pick a photo</Text>
-      </TouchableOpacity>
-    </View>
-  )
-
-} else {
-  return (
-    <View>
+          <Image
+          source={{uri: selectedProfileImage}}
+          style={styles.thumbnail}
+          />
       
-      
-      <Text>
-        To share a photo from your phone with a friend, just press the button below!
-      </Text>
+          <Text>
+            To share a photo from your phone with a friend, just press the button below!
+          </Text>
 
-      <TouchableOpacity onPress={openImagePickerAsync}>
-        <Text>Pick a photo</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+          <TouchableOpacity onPress={openImagePickerAsync}>
+            <Text>Pick a photo</Text>
+          </TouchableOpacity>
+
+         
+          <Button title="Download it" onPress={getDownloadImage}/>
+          
+
+        </View>
+      )
+
+    } else {
+      return (
+        <View>
+          <Text>
+            To share a photo from your phone with a friend, just press the button below!
+          </Text>
+          <Text>
+            {selectedProfileImage}
+          </Text>
+          <Button title="Download it" onPress={getDownloadImage}/>
+
+          <TouchableOpacity onPress={openImagePickerAsync}>
+            <Text>Pick a photo</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
 }
 const styles = StyleSheet.create({
     /* Other styles hidden to keep the example brief... */
